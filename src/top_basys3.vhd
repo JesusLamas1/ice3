4b0699cc9fe204d16aefaae24fc6bde121df0820
Your Name <c26jesus.lamas@afacademy.af.edu>
2025-02-13 22:37:47 -0700

    ice3 updated

--- src/top_basys3.vhd
+++ src/top_basys3.vhd
@@ -56,14 +56,40 @@ end top_basys3;
 architecture top_basys3_arch of top_basys3 is 
 	
     -- declare the component of your top-level design
+ component ripple_adder is
+           port (
+               A    : in std_logic_vector(3 downto 0);
+               B    : in std_logic_vector(3 downto 0);
+               Cin  : in std_logic;
+               S    : out std_logic_vector(3 downto 0);
+               Cout : out std_logic
+           );
+       end component ripple_adder;
 
     -- declare any signals you will need	
-  
+       signal w_A, w_B : std_logic_vector(3 downto 0);
+       signal w_Cin    : std_logic;
+       signal w_S      : std_logic_vector(3 downto 0);
+       signal w_Cout   : std_logic;
 begin
 	-- PORT MAPS --------------------
-   
+    w_A    <= sw(4 downto 1);  -- A from switches 4-1
+       w_B    <= sw(15 downto 12); -- B from switches 15-12
+       w_Cin  <= sw(0);           -- Cin from switch 0
+
 	---------------------------------
-	
+	ripple_adder_inst : ripple_adder
+            port map (
+                A    => w_A,
+                B    => w_B,
+                Cin  => w_Cin,
+                S    => w_S,
+                Cout => w_Cout
+            );
+  -- Drive LEDs with the sum and carry-out
+              led(3 downto 0) <= w_S;    -- Sum on LEDs 3-0
+              led(15) <= w_Cout;         -- Carry-out on LED 15
+
 	-- CONCURRENT STATEMENTS --------
 	led(14 downto 4) <= (others => '0'); -- Ground unused LEDs
 	---------------------------------
